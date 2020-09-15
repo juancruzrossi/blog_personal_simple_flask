d1a7b40eb077b0264f81b1111eda9f3b1bc8c806
juancruzrossi <68936439+juancruzrossi@users.noreply.github.com>
2020-09-15 12:16:05 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+
+#Configuracion de la BBDD
+USER_DB = 'postgres'
+PASS_DB = 'admin'
+URL_DB = 'localhost'
+NAME_DB = 'blog_db'
+FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Creamos el objeto db de SQLAlchemy
+db=SQLAlchemy(app)
+
+#Creacion de la tabla en la BBDD (La tabla se va a llamar blogpost en minusucula por default)
+class Blogpost(db.Model):
+    #Columnas de la tabla (todas las tablas deberian tener un ID)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+    
+    #date_posted = post.date_posted.strftime('%B %d, %Y')
+    
+    return render_template('post.html', post=post)
+    
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+    
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+    
+    db.session.add(post)
+    db.session.commit()
+    
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
